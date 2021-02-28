80888a8b155fc4724dfc0846d33521e988c23d29
Michelle <wuebben.michelle@yahoo.com>
2021-02-28 17:15:48 -0600

    moved jquery up

--- content/js/home-three.js
+++ content/js/home-three.js
@@ -1,7 +1,6 @@
 $(function() {
     $('.intro').addClass('go');
   
-    $('.reload').click(function() {
       $('.intro').removeClass('go').delay(200).queue(function(next) {
         $('.intro').addClass('go');
         next();
