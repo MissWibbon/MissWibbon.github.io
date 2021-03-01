425a98e2b30e55487eb98f626c8254909e4dabe8
Michelle <wuebben.michelle@yahoo.com>
2021-03-01 08:19:24 -0600

    made photo mobile responsive

--- content/js/home-three.js
+++ content/js/home-three.js
@@ -1,5 +1,5 @@
 $(function animateIntro() {
-	$('.intro').addClass('go');
+	$('.head .intro').addClass('go');
 	$('svg.intro .text').css('display', 'block');
 });
 
@@ -9,3 +9,22 @@ $(window).on('scroll', function() {
   $('.out').css({ '-webkit-filter': 'blur(' + pixs + 'px)', filter: 'blur(' + pixs + 'px)' });
   console.log(pixs)
 });
+
+
+
+setTimeout(function() {$(window).scroll(function() {
+  if (checkVisible($('svg.intro'))) {
+    $('svg.intro').addClass('go');
+  } 
+});},1000)
+
+function checkVisible( elm, eval ) {
+  eval = eval || "visible";
+  var vpH = $(window).height(), // Viewport Height
+      st = $(window).scrollTop(), // Scroll Top
+      y = $(elm).offset().top,
+      elementHeight = $(elm).height();
+  
+  if (eval == "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
+  if (eval == "above") return ((y < (vpH + st)));
+}
