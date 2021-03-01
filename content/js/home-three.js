4e6ad951b58b8c2e47076dd51f2b1bd2ca39838a
Michelle <wuebben.michelle@yahoo.com>
2021-02-28 19:19:32 -0600

    added wrapper

--- content/js/home-three.js
+++ content/js/home-three.js
@@ -1,4 +1,11 @@
 $(function animateIntro() {
-    $('.intro').addClass('go');
-    $('svg.intro .text').css('display', 'block')
-  })
+	$('.intro').addClass('go');
+	$('svg.intro .text').css('display', 'block');
+});
+
+$(window).on('scroll', function() {
+	var pixs = $(document).scrollTop();
+	pixs = pixs / 100;
+  $('.out').css({ '-webkit-filter': 'blur(' + pixs + 'px)', filter: 'blur(' + pixs + 'px)' });
+  console.log(pixs)
+});
